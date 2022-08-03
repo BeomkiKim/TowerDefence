08c948123cd456b654a2cb0c185f09188a938692
BeomkiKim <bumki1226@gmail.com>
2022-08-03 22:13:10 +0900

    타워, 몬스터
    타워가 몬스터를 따라가게,
    몬스터 에셋찾음, 타워에셋 찾음

---
+++ Defense/Assets/2. Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+    Transform target;
+    int wavepointIndex = 0;
+
+    private void Start()
+    {
+        target = WayPoints.points[0];
+
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized*speed *Time.deltaTime,Space.World);
+
+        if(Vector3.Distance(transform.position, target.position)<= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+
+    }
+
+    void GetNextWaypoint()
+    {
+        if(wavepointIndex >= WayPoints.points.Length -1)
+        {
+            Destroy(gameObject);
+        }
+        wavepointIndex++;
+        target = WayPoints.points[wavepointIndex];
+    }
+}
